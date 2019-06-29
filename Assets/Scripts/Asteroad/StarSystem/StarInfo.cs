9d466d983abc0408caf0fc3b2b45ff1a1d23c336
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-06-30 02:52:18 +0900

    素材追加

---
+++ Assets/Scripts/Asteroad/StarSystem/StarInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroad
+{
+    namespace StarSystem
+    {
+        public class StarInfo : MonoBehaviour
+        {
+            [SerializeField] private int starID = 1;            // 星のID
+            [SerializeField] private GameObject ActiveMarker;   // アクティブ状態を表すマーカーUI
+
+            public int StarID { get { return this.starID; } }
+
+            public void Activate()
+            {
+                ActiveMarker.SetActive(true);
+            }
+
+            public void DisActivate()
+            {
+                ActiveMarker.SetActive(false);
+            }
+        }
+    }
+}
